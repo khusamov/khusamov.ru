37cf2fbf46f08a969d7f4a6add66ed4bc12b2ddc
Khusamov Sukhrob <khusamov@yandex.ru>
2019-02-13 19:36:48 +0300

    Добавлен пример fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
 import HomeLayout from '../layouts/HomeLayout';
+import fetch from 'isomorphic-unfetch';
 
-export default class extends Component {
+interface IIndexPageProps {
+	batmanShows: any[];
+}
+
+export default class IndexPage extends Component<IIndexPageProps> {
+	static async getInitialProps(): Promise<IIndexPageProps> {
+		const batmanShowsResponse = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+		const batmanShows = await batmanShowsResponse.json();
+		return {batmanShows};
+	}
 	render() {
 		return (
 			<HomeLayout title='khusamov.ru'>
 				<p>Привет!</p>
+				{this.props.batmanShows.map((item: any, index: number) => <div key={index}>{item.show.name}</div>)}
 			</HomeLayout>
 		);
 	}
