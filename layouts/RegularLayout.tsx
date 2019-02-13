4736678d2dd0d8bdbe5d022740045afb4fe892d2
Khusamov Sukhrob <khusamov@yandex.ru>
2019-02-13 18:57:37 +0300

    Добавлена заметка: Вместо Helmet можно заюзать next/head

--- layouts/RegularLayout.tsx
+++ layouts/RegularLayout.tsx
@@ -6,6 +6,9 @@ interface IRegularLayoutProps {
 	title: string;
 }
 
+// Вместо Helmet можно заюзать next/head.
+// https://github.com/zeit/next.js/blob/master/examples/with-typescript/components/Layout.tsx
+
 export default class RegularLayout extends Component<IRegularLayoutProps> {
 	render() {
 		return (
