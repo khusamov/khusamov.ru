30b727296e37e21c2e2275aca67d1a9c68bcba29
Khusamov Sukhrob <khusamov@yandex.ru>
2019-02-14 15:22:31 +0300

    Html: Добавлена ссылка https://reactjs.org/docs/dom-elements.html#dangerouslysetinnerhtml

--- components/Html.tsx
+++ components/Html.tsx
@@ -1,6 +1,8 @@
 import React, {FunctionComponent} from 'react';
+
 /**
  * Специальный компонент для вывода HTML-кода без экранирования.
+ * @link https://reactjs.org/docs/dom-elements.html#dangerouslysetinnerhtml
  */
 const Html: FunctionComponent = ({children}) => (
 	<div dangerouslySetInnerHTML={{__html: children as string}}/>
