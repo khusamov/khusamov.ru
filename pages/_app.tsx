987e93ade4389f3921543f375ce2cf9a6f9abd0d
Khusamov Sukhrob <khusamov@yandex.ru>
2019-02-14 14:24:47 +0300

    Добавлен файл с глобальными стилями

---
+++ pages/_app.tsx
@@ -0,0 +1,28 @@
+import App from 'next/app';
+import '../global.scss';
+export default App;
+
+// https://github.com/zeit/next.js/issues/5425
+// import React from 'react';
+// import App, {Container, NextAppContext, DefaultAppIProps} from 'next/app';
+// export default class MyApp extends App {
+// 	static async getInitialProps(context: NextAppContext): Promise<DefaultAppIProps> {
+// 		const {Component, ctx} = context;
+// 		let pageProps = {};
+//
+// 		if (Component.getInitialProps) {
+// 			pageProps = await Component.getInitialProps(ctx);
+// 		}
+//
+// 		return { pageProps };
+// 	}
+//
+// 	render() {
+// 		const { Component, pageProps } = this.props;
+// 		return (
+// 			<Container>
+// 				<Component {...pageProps} />
+// 			</Container>
+// 		);
+// 	}
+// }
