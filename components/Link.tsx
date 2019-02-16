c2516374fdd0264f580f39cca8a9e8eaf6878073
Khusamov Sukhrob <khusamovsa@it.mos.ru>
2019-02-16 19:05:40 +0300

    Link: Исправлено предупреждение: Warning: Failed prop type: Link: unknown props found: router.

--- components/Link.tsx
+++ components/Link.tsx
@@ -5,16 +5,45 @@ import {withRouter, WithRouterProps} from 'next/router';
 type TLinkProps = WithRouterProps & NextLinkProps;
 
 /**
+ * Перечень ключей интерфейса NextLinkProps.
+ */
+const TNextLinkPropKeys = [
+	'prefetch', 'shallow', 'scroll', 'replace',
+	'href', 'as', 'passHref', 'children'
+];
+
+/**
  * Замена штатного next/link на свой компонент Link,
  * который убирает ссылку, если href совпадает с текущим URL.
  */
 const Link = withRouter(
-	class Link extends Component<TLinkProps> {
-		render() {
+	class LinkWithoutRouter extends Component<TLinkProps> {
+		/**
+		 * В компонент NextLink пропускаем только пропсы из NextLinkProps.
+		 * Иными словами, пропсы WithRouterProps отфильтровываем.
+		 * Эти исключает предупреждение вида:
+		 * Warning: Failed prop type: Link: unknown props found: router.
+		 * @param linkProps
+		 */
+		private static filterNextLinkProps(linkProps: TLinkProps): NextLinkProps {
+			const nextLinkProps: NextLinkProps = {children: linkProps.children};
+			let prop: keyof TLinkProps;
+			for (prop in linkProps) {
+				if (
+					linkProps.hasOwnProperty(prop)
+					&& TNextLinkPropKeys.indexOf(prop) !== -1
+				) {
+					nextLinkProps[prop as keyof NextLinkProps] = linkProps[prop];
+				}
+			}
+			return nextLinkProps;
+		}
+
+		public render() {
 			return (
 				this.props.router && this.props.router.pathname === this.props.href
 					? this.props.children
-					: <NextLink {...this.props}>{this.props.children}</NextLink>
+					: <NextLink {...LinkWithoutRouter.filterNextLinkProps(this.props)}>{this.props.children}</NextLink>
 			);
 		}
 	}
