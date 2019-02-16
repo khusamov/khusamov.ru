ac838fa1f8f68b6baeee7e667dc8d6017d9ba0cd
Khusamov Sukhrob <khusamovsa@it.mos.ru>
2019-02-16 12:04:49 +0300

    Menu: Пункт меню выделены в отдельный массив данных

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,14 +1,34 @@
 import React, {Component, Fragment} from 'react';
 import Link from './Link';
 
+interface IItem {
+	title: string;
+	uri: string;
+}
+
 export default class Menu extends Component {
+	private items: IItem[] = [{
+		title: 'Главная',
+		uri: '/'
+	}, {
+		title: 'О сайте',
+		uri: '/about'
+	}, {
+		title: 'Инструмент',
+		uri: '/tool'
+	}];
 	render() {
-		return (
+		return this.items.map(({title, uri}, index) => (
 			<Fragment>
-				<Link href='/'><a>Главная</a></Link>
-				<span> | </span>
-				<Link href='/about'><a>О сайте</a></Link>
+				<Link key={index} href={uri}>
+					<a>{title}</a>
+				</Link>
+				{
+					index !== this.items.length - 1 && (
+						<span> | </span>
+					)
+				}
 			</Fragment>
-		);
+		));
 	}
 }
