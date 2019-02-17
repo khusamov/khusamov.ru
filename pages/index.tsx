aa004b6a113d20106d428784a340881e66fcae58
Khusamov Sukhrob <khusamovsa@it.mos.ru>
2019-02-17 20:23:35 +0300

    pages/index: Список ссылок обернут в параграф

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,22 +8,26 @@ interface IIndexPageProps {
 }
 
 export default class IndexPage extends Component<IIndexPageProps> {
-	static async getInitialProps(): Promise<IIndexPageProps> {
+	public static async getInitialProps(): Promise<IIndexPageProps> {
 		const batmanShowsResponse = await fetch('https://api.tvmaze.com/search/shows?q=batman');
 		const batmanShows = await batmanShowsResponse.json();
 		return {batmanShows};
 	}
-	render() {
+	public render() {
 		return (
 			<HomeLayout title='khusamov.ru'>
 				<p>Batman Shows:</p>
-				{this.props.batmanShows.map((item: any, index: number) => (
-					<div key={index}>
-						<Link href={`/post?id=${item.show.id}`} as={`/post/${item.show.id}`}>
-							<a>{item.show.name}</a>
-						</Link>
-					</div>
-				))}
+				<p>
+					{
+						this.props.batmanShows.map((item: any, index: number) => (
+							<div key={index}>
+								<Link href={`/post?id=${item.show.id}`} as={`/post/${item.show.id}`}>
+									<a>{item.show.name}</a>
+								</Link>
+							</div>
+						))
+					}
+				</p>
 			</HomeLayout>
 		);
 	}
