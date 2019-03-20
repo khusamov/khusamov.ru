e6f400e3ed1185d64b9a4f690dc9afd852562c68
Khusamov Sukhrob <khusamov@yandex.ru>
2019-03-20 14:53:57 +0300

    Добавлена возможность вставлять файлы с глобальными стилями из node_modules

--- next.config.js
+++ next.config.js
@@ -1,11 +1,26 @@
 const withTypescript = require('@zeit/next-typescript');
 const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css');
+const Base64 = require('base-64');
+
 module.exports = withTypescript(withCSS(withSass({
 	cssModules: true,
 	cssLoaderOptions: {
 		importLoaders: 1,
-		localIdentName: "[local]___[hash:base64:5]",
+		/**
+		 * Функция getLocalIdent генерирует локальные имена для всех файлов css|scss
+		 * кроме тех, кто находится в node_modules.
+		 * Это сделано для того, чтобы добавить глобальные стили из файла:
+		 * node_modules/highlight.js/styles/github.css
+		 */
+		getLocalIdent(loaderContext, localIdentName, localName, options) {
+			const hash = Base64.encode(loaderContext.resourcePath.replace(__dirname, ''));
+			return (
+				loaderContext.resourcePath.indexOf('node_modules') === -1
+					? localName + '___' + hash.substr(-10)
+					: localName
+			);
+		}
 	},
 	webpack(config, options) {
 		config.module.rules.push({
