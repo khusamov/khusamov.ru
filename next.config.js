ced0d2e03d7e3fe5b8db9d4eeec61bd4ee5fafb8
Khusamov Sukhrob <khusamov@yandex.ru>
2019-04-02 18:07:30 +0300

    next.config.js: Упрощение кода

--- next.config.js
+++ next.config.js
@@ -1,33 +1,42 @@
-const withTypescript = require('@zeit/next-typescript');
-const withSass = require('@zeit/next-sass');
-const withCSS = require('@zeit/next-css');
 const Base64 = require('base-64');
+const withCSS = require('@zeit/next-css');
+const withSass = require('@zeit/next-sass');
+const withTypescript = require('@zeit/next-typescript');
 
+/**
+ * Конфигурация Next.
+ * @link https://github.com/zeit/next.js#custom-configuration
+ */
 module.exports = withTypescript(withCSS(withSass({
+	cssLoaderOptions: {getLocalIdent, importLoaders: 1},
 	cssModules: true,
-	cssLoaderOptions: {
-		importLoaders: 1,
-		/**
-		 * Функция getLocalIdent генерирует локальные имена для всех файлов css|scss
-		 * кроме тех, кто находится в node_modules.
-		 * Это сделано для того, чтобы добавить глобальные стили из файла:
-		 * node_modules/highlight.js/styles/github.css
-		 */
-		getLocalIdent(loaderContext, localIdentName, localName, options) {
-			const hash = Base64.encode(loaderContext.resourcePath.replace(__dirname, ''));
-			return (
-				loaderContext.resourcePath.indexOf('node_modules') === -1
-					? localName + '___' + hash.substr(-10)
-					: localName
-			);
-		}
-	},
-	webpack(config, options) {
-		config.module.rules.push({
-			test: /\.md$/,
-			loader: './loaders/markdownLoader'
-
-		});
-		return config;
-	}
+	webpack: webpackHandler
 })));
+
+/**
+ * Изменение конфигурации Webpack.
+ * @link https://github.com/zeit/next.js#customizing-webpack-config
+ */
+function webpackHandler(config, options) {
+	config.module.rules.push({
+		loader: './loaders/markdownLoader',
+		test: /\.md$/
+	});
+	return config;
+}
+
+/**
+ * Функция getLocalIdent генерирует локальные имена для всех файлов css|scss
+ * кроме тех, кто находится в node_modules.
+ * Это сделано для того, чтобы добавить глобальные стили из файла:
+ * node_modules/highlight.js/styles/github.css
+ * @link https://github.com/webpack-contrib/css-loader#getlocalident
+ */
+function getLocalIdent(loaderContext, localIdentName, localName, options) {
+	const hash = Base64.encode(loaderContext.resourcePath.replace(__dirname, ''));
+	return (
+		loaderContext.resourcePath.indexOf('node_modules') === -1
+			? localName + '___' + hash.substr(-10)
+			: localName
+	);
+}
