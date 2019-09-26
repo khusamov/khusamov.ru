2bd0986b674347f81deab4cbf7d03656a132b18e
Khusamov Sukhrob <khusamov@yandex.ru>
2019-09-26 15:00:04 +0300

    Добавлен packages/particles.khusamov.ru

---
+++ packages/particles.khusamov.ru/README.md
@@ -0,0 +1,19 @@
+particles.khusamov.ru
+=====================
+
+Временная красивая заглушка для `khusamov.ru.`
+
+Фон из точек и линий, анимированный
+----------------------------------
+
+Фон сделан на основе 
+https://github.com/VincentGarreau/particles.js
+
+Визуальный редактор настроек `particles-js`
+https://vincentgarreau.com/particles.js/
+
+Компонент для Реакта
+https://github.com/Wufe/react-particles-js
+
+Описание, как сделать снегопад на основе `particles-js`
+https://pcvector.net/485-snegopad-na-baze-particlesjs.html
---
+++ packages/particles.khusamov.ru/src/App.tsx
@@ -0,0 +1,64 @@
+import React, {CSSProperties} from 'react';
+import './App.scss';
+import Particles, {IParticlesParams} from 'react-particles-js';
+import config from './particlesjs-config.json';
+import Helmet from 'react-helmet';
+import packageJson from '../package.json';
+
+const blogs = [{
+	name: 'Поселок Радужный',
+	url: 'https://vk.com/the_village_rainbow'
+}, {
+	name: 'Бытовка своими руками',
+	url: 'https://vk.com/shed_hand_made'
+}, {
+	name: 'NodeJS',
+	url: 'https://vk.com/node_javascript'
+}, {
+	name: 'ReactJS',
+	url: 'https://vk.com/react_javascript'
+}, {
+	name: 'JavaScript для браузеров',
+	url: 'https://vk.com/javascript_for_web'
+}, {
+	name: 'TypeScript',
+	url: 'https://vk.com/mytypescript'
+}, {
+	name: 'Sencha Ext JS',
+	url: 'https://vk.com/sencha_extjs'
+}];
+
+
+const footerStyle: CSSProperties = {
+	position: 'absolute',
+	bottom: 0,
+	left: 0,
+	padding: '8px 15px',
+	backgroundColor: '#fff0f0',
+	fontSize: '80%'
+};
+
+const App: React.FC = () => {
+	return (
+		<div className="App">
+			<Helmet>
+				<title>khusamov.ru</title>
+			</Helmet>
+			<Particles style={{position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, zIndex: -1}} params={config as IParticlesParams}/>
+			<h1>khusamov.ru</h1>
+			<div>ВКонтакте: <a href='https://vk.com/crystalset'>https://vk.com/crystalset</a></div>
+			<div>GitHub: <a href='https://github.com/khusamov'>https://github.com/khusamov</a></div>
+			<div>Скайп: <a href='skype:khusamov.ru'>khusamov.ru</a></div>
+
+			<br/>
+			<div>Мои микроблоги:</div>
+			{blogs.map(blog => <div><a target='_blank' href='{blog.url}'>{blog.name}</a></div>)}
+
+			<div style={footerStyle}>
+				Версия сайта {packageJson.version}
+			</div>
+		</div>
+	);
+};
+
+export default App;
